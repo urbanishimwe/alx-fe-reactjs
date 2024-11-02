11cf9ce5274b986d77c601c79aa8df387d82d330
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-02 10:37:36 +0200

    5

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,9 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from "./components/UserProfile";
 
 function App() {
 
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
   );
 }
 
