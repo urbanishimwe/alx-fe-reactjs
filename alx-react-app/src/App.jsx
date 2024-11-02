031e4cea8ad0066da7f921bfedb8a01b1107a860
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-02 10:34:28 +0200

    4

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
-    <WelcomeMessage />
-  )
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
 export default App
