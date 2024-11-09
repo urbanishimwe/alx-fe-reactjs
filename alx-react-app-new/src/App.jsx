16cb05968860d28629de6649eb6c04aa83ff8df1
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-09 10:10:38 +0200

    7

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <Counter/>
       <Footer />
     </>
   );
