bff1b56c5405eb530cb893dd01bf81c21a3c5f07
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-09 10:44:05 +0200

    9

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from './components/ProfilePage'
-import { UserContext } from './context/UserContext'
+import { UserContext } from './UserContext'
 
 const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
