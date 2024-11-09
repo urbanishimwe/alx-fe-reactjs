78c7d456851b20b6b364a6d04b0b05837958dbd0
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-09 12:08:13 +0200

    19

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Navbar() {
     return (
         <>
-            <ul style={{ backgroundColor: "black", color: "white" }}>
+            <ul style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
                 <li> <Link to="/home"> Home </Link></li>
                 <li> <Link to="/services"> Services</Link> </li>
                 <li> <Link to="/contact"> Contact us</Link> </li>
