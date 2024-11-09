e79afa5acd55397bc7b671ba025a590f484208fe
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-09 12:02:00 +0200

    15

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,13 +1,29 @@
-import {Outlet} from 'react-router-dom';
-import NavBar  from './components/NavBar';
+import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from 'react-router-dom';
+import Home from "./components/Home"
+import About from './components/About'
+import Contact from './components/Contact'
+import Services from './components/Services'
+
+export const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App />}>
+    <Route path='' element={<Home />} />
+    <Route path='home' element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='contact' element={<Contact />} />
+    <Route path='services' element={<Services />} />
+    <Route path='*' element={<h1> Not found </h1>} />
+  </Route>
+));
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <Outlet/>
-    <Footer/>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
