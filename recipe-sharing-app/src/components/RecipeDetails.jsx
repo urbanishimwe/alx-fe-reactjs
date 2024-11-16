8362fd00874c33a6536d06e8604749978e028fd5
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-16 11:13:00 +0200

    25

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,12 +1,14 @@
 import EditRecipeForm from './EditRecipeForm'
 import DeleteRecipeButton from './DeleteRecipeButton'
+import { useRecipeStore } from './recipeStore';
 
 const RecipeDetails = ({ recipeId }) => {
+    const recipe = useRecipeStore(state => state.recipes.find(recipe => recipe.id === recipeId));
     return (
         <div>
             <h1> Edit Recipe </h1>
-            <EditRecipeForm recipeId={recipeId}/>
-            <DeleteRecipeButton recipeId={recipeId}/>
+            <EditRecipeForm recipeId={recipe.id}/>
+            <DeleteRecipeButton recipeId={recipe.id}/>
         </div >
     );
 };
