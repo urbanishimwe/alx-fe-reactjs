31e62b793619fffbe74e824ce3d194eaca86c933
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-16 09:16:23 +0200

    21

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+  return (
+    <>
+      <AddRecipeForm/>
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
