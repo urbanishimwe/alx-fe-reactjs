473170e42bf3e8c711666469324f73988aa41a5c
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-16 11:41:18 +0200

    28

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
-import RecipeView from './components/RecipeView'
+import RecipeDetails from './components/RecipeDetails'
 import Navbar from './components/Navbar'
 import { createBrowserRouter, createRoutesFromElements, Route, Outlet } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ export const Router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<App />}>
     <Route path='recipe/add' element={< AddRecipeForm />} />
     <Route path='recipe/list' element={< RecipeList />} />
-    <Route path='recipe/view/*' element={<RecipeView />} />
+    <Route path='recipe/view/*' element={<RecipeDetails />} />
     <Route path='*' element={<h1> Not found </h1>} />
   </Route>
 ));
--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -2,14 +2,22 @@ import EditRecipeForm from './EditRecipeForm'
 import DeleteRecipeButton from './DeleteRecipeButton'
 import { useRecipeStore } from './recipeStore';
 
-const RecipeDetails = ({ recipeId }) => {
+const RecipeDetails = () => {
+    const path = window.location.pathname || "";
+    const recipeId = parseInt(path.substring(path.lastIndexOf('/') + 1));
     const recipe = useRecipeStore(state => state.recipes.find(recipe => recipe.id === recipeId));
+    if (recipe) {
+        return (
+            <div>
+                <h1> Edit Recipe </h1>
+                <EditRecipeForm recipeId={recipe.id} />
+                <DeleteRecipeButton recipeId={recipe.id} />
+            </div >
+        );
+    }
+
     return (
-        <div>
-            <h1> Edit Recipe </h1>
-            <EditRecipeForm recipeId={recipe.id}/>
-            <DeleteRecipeButton recipeId={recipe.id}/>
-        </div >
+        <h1> Recipe not found </h1>
     );
 };
 
