c9c5a54218b3c58137065e98e50ca395a35514fc
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-22 18:56:04 +0200

    40

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from 'react-query';
 
-const fetchData = async () => {
+const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     return res.json();
 };
 
 const PostsComponent = () => {
-    const { data, error, isLoading } = useQuery('fetchData', fetchData);
+    const { data, isError, isLoading } = useQuery('fetchPosts', fetchPosts);
 
     if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error loading data</div>;
+    if (isError) return <div>Error loading data</div>;
 
     return (
         <div>
