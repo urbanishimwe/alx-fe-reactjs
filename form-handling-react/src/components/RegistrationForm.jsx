297860ade10de82bfc521fe9f6140c92473bdad4
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-11-22 18:39:33 +0200

    36

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,8 +7,23 @@ const ControlledForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        var errors = [];
+        if(!username) {
+            errors.push("username is required")
+        }
+        if(!email) {
+            errors.push("email is required")
+        }
+        if(!password) {
+            errors.push("password is required")
+        }
+        setErrors(errors)
         console.log(username, email, password);
     };
+
+    const setErrors = (errors) => {
+        document.getElementById("errors").innerHTML = errors.join("<br/>")
+    }
 
     return (
         <form onSubmit={handleSubmit}>
@@ -30,6 +45,7 @@ const ControlledForm = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
+            <p id="errors"> </p>
             <button type="submit">Submit</button>
         </form>
     );
