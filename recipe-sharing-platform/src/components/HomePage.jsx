2b9a7ccf91b690d9ca11e6f3aa68759b89e67fb4
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-12-07 08:37:09 +0200

    65

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
 
     console.log(recipes);
     return (
-        <>
+        <div className='grid grid-cols-1 grid-cols-2 grid-cols-3'>
             {recipes.map(e =>
                 <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto mx-auto rounded-lg shadow-lg my-20 hover:shadow-xl" key={e.id}>
                     <img src={e.image} className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" alt="User" />
@@ -18,7 +18,7 @@ const HomePage = () => {
                     <p className="text-gray-600 md:text-base sm:text-sm hover:text-blue-500"> {e.summary} </p>
                 </div>
             )}
-        </>
+        </div>
     )
 }
 
