9b401db6321a0f1cde3d3053efb43ba370d53338
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-12-07 08:45:27 +0200

    66

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import recipesList from '../data.json'
+import { NavLink } from 'react-router-dom';
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState(recipesList);
@@ -12,11 +13,11 @@ const HomePage = () => {
     return (
         <div className='grid grid-cols-1 grid-cols-2 grid-cols-3'>
             {recipes.map(e =>
-                <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto mx-auto rounded-lg shadow-lg my-20 hover:shadow-xl" key={e.id}>
+                <NavLink className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto mx-auto rounded-lg shadow-lg my-20 hover:shadow-xl" key={e.id} to={`/recipe/${e.id}`}>
                     <img src={e.image} className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" alt="User" />
                     <h1 className="md:text-xl sm:text-lg text-blue-800 my-4"> {e.title} </h1>
                     <p className="text-gray-600 md:text-base sm:text-sm hover:text-blue-500"> {e.summary} </p>
-                </div>
+                </NavLink>
             )}
         </div>
     )
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+import recipesList from '../data.json'
+
+const RecipeDetail = () => {
+    const id = parseInt(window.location.pathname.split('/')[2]);
+
+    const recipe = recipesList.find(e => e.id === id);
+    if (!recipe) {
+        return <h1> Not found </h1>
+    }
+
+    return (
+        <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto mx-auto rounded-lg shadow-lg my-20 hover:shadow-xl" key={recipe.id}>
+            <img src={recipe.image} className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" alt="User" />
+            <h1 className="md:text-xl sm:text-lg text-blue-800 my-4"> {recipe.title} </h1>
+            <p className="text-gray-600 md:text-base sm:text-sm hover:text-blue-500"> {recipe.summary} </p>
+        </div>
+    )
+}
+
+export default RecipeDetail;
